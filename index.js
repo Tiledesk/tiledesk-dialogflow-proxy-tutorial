bf4126da38ba0a3c74f482f87b2c48ddc24b28a8
Andrea Sponziello <andrea.sponziello@gmail.com>
2020-02-29 10:30:57 +0100

    testing

--- index.js
+++ index.js
@@ -45,6 +45,7 @@ app.post("/bot", (req, res) => {
   const dialogflow_session_id = conversation.request_id
   const lang = 'en-EN'
   const credentials = process.env.GOOGLE_CREDENTIALS
+  console.log("CREDENTIALS:", credentials)
   runDialogflowQuery(tdclient.text, dialogflow_session_id, lang, credentials)
   .then(function(result) {
     if(res.statusCode === 200) {
