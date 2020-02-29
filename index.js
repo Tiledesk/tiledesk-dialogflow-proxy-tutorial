b01316b62ef5b61cee321e09a68e04209b38d126
Andrea Sponziello <andrea.sponziello@gmail.com>
2020-02-29 10:21:38 +0100

    credentials from process.env

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { TiledeskClient } = require('@tiledesk/tiledesk-chatbot-client');
 const dialogflow = require('dialogflow');
-const credentials = require('./credentials.js');
 
 const app = express();
 app.use(bodyParser.json());
@@ -45,6 +44,7 @@ app.post("/bot", (req, res) => {
 
   const dialogflow_session_id = conversation.request_id
   const lang = 'en-EN'
+  const credentials = process.env.GOOGLE_CREDENTIALS
   runDialogflowQuery(tdclient.text, dialogflow_session_id, lang, credentials)
   .then(function(result) {
     if(res.statusCode === 200) {
