3727a5a914edfb904ac5d707dba6b9329fffd2f4
Andrea Sponziello <andrea.sponziello@gmail.com>
2020-02-29 12:18:33 +0100

    test

--- index.js
+++ index.js
@@ -2,20 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { TiledeskClient } = require('@tiledesk/tiledesk-chatbot-client');
 const dialogflow = require('dialogflow');
-
 const app = express();
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello Tiledesk-Dialogflow proxy!')
-});
-
 async function runDialogflowQuery(text, sessionId, language_code, credentials) {
-
   const project_id = credentials.project_id
   const sessionClient = new dialogflow.SessionsClient({'credentials':credentials});
   const sessionPath = sessionClient.sessionPath(project_id, sessionId);
-  
   var request;
   request = {
     session: sessionPath,
@@ -26,31 +19,24 @@ async function runDialogflowQuery(text, sessionId, language_code, credentials) {
       }
     }
   };
-  // Send request and log result
   const responses = await sessionClient.detectIntent(request);
   const result = responses[0].queryResult;
   return result;
 }
 
 app.post("/bot", (req, res) => {
-  console.log("BOT: req.body: " + JSON.stringify(req.body));
-  
   const tdclient = new TiledeskClient({request: req, response: res});
-
   let conversation = tdclient.conversation
-
   // immediately reply back
   res.status(200).send({"success":true});
-
+  // reply messages are sent asynchronously
   const dialogflow_session_id = conversation.request_id
   const lang = 'en-EN'
   const credentials = JSON.parse(process.env.GOOGLE_CREDENTIALS)
-  console.log("CREDENTIALS:", credentials)
   runDialogflowQuery(tdclient.text, dialogflow_session_id, lang, credentials)
   .then(function(result) {
     if(res.statusCode === 200) {
       const reply_text = result['fulfillmentText']
-      console.log("reply:", reply_text)
       var msg_attributes = {}
       var msg = {
         "text": reply_text,
@@ -63,7 +49,7 @@ app.post("/bot", (req, res) => {
     }
   })
   .catch(function(err) {
-    console.log('BOT: error: ', err);
+    console.log('Error: ', err);
   })
 })
 
