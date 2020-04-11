606ac99b384df74c80ecfc6706e0c7efd409f4ab
Andrea Sponziello <andrea.sponziello@gmail.com>
2020-04-11 11:52:49 +0200

    updated with microlang example

--- index.js
+++ index.js
@@ -53,21 +53,24 @@ app.post("/bot/:botid", (req, res) => {
         console.log("Message sent.");
       })
     }
-  }) 
+  })
   .catch(function(err) {
     console.log('Error: ', err);
   })
 })
 
-app.post("/microlang-bot", (req, res) => {
+app.post("/microlang-bot/:botid", (req, res) => {
   const tdclient = new TiledeskClient({request: req, response: res});
-  let conversation = tdclient.conversation
+  const botid = req.params.botid;
+  console.log("botid:", botid)
+  const conversation = tdclient.conversation
   // immediately reply back
   res.status(200).send({"success":true});
   // reply messages are sent asynchronously
   const dialogflow_session_id = conversation.request_id
   const lang = 'en-EN' // lang must be the same of the Dialogflow Agent
-  const credentials = JSON.parse(process.env.GOOGLE_CREDENTIALS)
+  console.log("loading credentials...") //, process.env[botid])
+  const credentials = JSON.parse(process.env[botid])
   runDialogflowQuery(tdclient.text, dialogflow_session_id, lang, credentials)
   .then(function(result) {
     console.log("query result: ", JSON.stringify(result))
@@ -77,12 +80,57 @@ app.post("/microlang-bot", (req, res) => {
     if(res.statusCode === 200) {
       const reply_text = result['fulfillmentText']
       const parsed_reply = new TiledeskChatbotUtil().parseReply(reply_text)
-      const parsed_message = parsed_reply.message
+      const msg = parsed_reply.message
+      // NOTE: you can also use parts of the parsed message, like this
+      // var msg = {
+      //   "text": parsed_message.text,
+      //   "type": parsed_message.type,
+      //   "attributes": msg_attributes,
+      //   "metadata": parsed_message.metadata,
+      //   "senderFullname": tdclient.botName
+      // }
+      tdclient.sendMessage(msg, function (err) {
+        console.log("Message sent.");
+      })
+    }
+  })
+  .catch(function(err) {
+    console.log('Error: ', err);
+  })
+})
+
+var fallback_count = {}
+
+app.post("/bot-confidence-handoff/:botid", (req, res) => {
+  const tdclient = new TiledeskClient({request: req, response: res});
+  const botid = req.params.botid;
+  const conversation = tdclient.conversation
+  // immediately reply back
+  res.status(200).send({"success":true});
+  // reply messages are sent asynchronously
+  const dialogflow_session_id = conversation.request_id
+  const lang = 'en-EN' // lang must be the same of the Dialogflow Agent
+  const credentials = JSON.parse(process.env[botid])
+  runDialogflowQuery(tdclient.text, dialogflow_session_id, lang, credentials)
+  .then(function(result) {
+    console.log("query result: ", JSON.stringify(result))
+    console.log("is fallback:", result.intent.isFallback)
+    if (result.intent.isFallback) {
+      if (!fallback_count[dialogflow_session_id]) {
+        fallback_count[dialogflow_session_id] = 1
+      }
+      else {
+        fallback_count[dialogflow_session_id]++
+      }
+    }
+    console.log("confidence:", result.intentDetectionConfidence)
+    // intentDetectionConfidence
+    if(res.statusCode === 200) {
+      const reply_text = result['fulfillmentText']
+      var msg_attributes = {}
       var msg = {
-        "text": parsed_message.text,
-        "type": parsed_message.type,
-        "attributes": msg_attributes,
-        "metadata": parsed_message.metadata,
+        "text": reply_text,
+        "type": "text",
         "senderFullname": tdclient.botName
       }
       tdclient.sendMessage(msg, function (err) {
@@ -94,17 +142,6 @@ app.post("/microlang-bot", (req, res) => {
     console.log('Error: ', err);
   })
 })
-// Use something like this to allow the same endpoint
-// to serve multiple bots
-app.post("/multibot/:agentid", (req, res) => {
-  // const agentid = req.params.agentid;
-  // ...
-
-  // Place in env an entry for each agent based on whatever
-  // id is useful to correctly identify the agent
-  const credentials = JSON.parse(process.env[agentid])
-  // ...
-});
 
 var port = process.env.PORT || 3000;
 app.listen(port, () => {
